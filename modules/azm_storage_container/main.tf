10364598103ef33acdb677eacae44ba2420c4767
Felipe Rios <felipe.rios.silva@outlook.com>
2023-11-09 12:08:03 -0500

    hotfix(storage): corrected the storage account without the new parameters
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

--- modules/azm_storage_container/main.tf
+++ modules/azm_storage_container/main.tf
@@ -14,6 +14,8 @@ resource "azurerm_storage_account" "default" {
   location                 = azurerm_resource_group.default.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+  allow_nested_items_to_be_public = false
 
   tags = merge(
     { Name = var.storage_account_name },
